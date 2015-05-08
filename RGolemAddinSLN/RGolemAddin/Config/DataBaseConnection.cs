5632cdabc1a80a3b6795fc052b621edee6aaa888
pospieszja <j.pospieszynski@gmail.com>
2015-05-08 14:50:58 +0200

    Added DataBaseConnection class

---
+++ RGolemAddinSLN/RGolemAddin/Config/DataBaseConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGolemAddin.Config
+{
+    static class DataBaseConnection
+    {
+        static string connectionString =
+                                "User=SYSDBA;" +
+                                "Password=masterkey;" +
+                                "Database=golem_data;" +
+                                "DataSource=192.168.5.70;" +
+                                "Port=3050";
+
+        public static string GetConnectionString()
+        {
+            return connectionString;
+        }
+    }
+}

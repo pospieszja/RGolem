830fa47afd29cc14ea410c33ac4ecfb141c067b8
pospieszja <j.pospieszynski@gmail.com>
2015-05-12 09:40:39 +0200

    Dodanie okna z parametrami raportu

---
+++ RGolemAddinSLN/RGolemAddin/View/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace RGolemAddin.View
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dateTimePicker4 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cbxListMachine = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dateTimePicker4);
+            this.groupBox1.Controls.Add(this.dateTimePicker3);
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(269, 116);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Zakres dat";
+            // 
+            // dateTimePicker4
+            // 
+            this.dateTimePicker4.CustomFormat = "HH:mm";
+            this.dateTimePicker4.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker4.Location = new System.Drawing.Point(137, 76);
+            this.dateTimePicker4.MaxDate = new System.DateTime(2100, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker4.MinDate = new System.DateTime(2015, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker4.Name = "dateTimePicker4";
+            this.dateTimePicker4.ShowUpDown = true;
+            this.dateTimePicker4.Size = new System.Drawing.Size(125, 20);
+            this.dateTimePicker4.TabIndex = 5;
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.CustomFormat = "HH:mm";
+            this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker3.Location = new System.Drawing.Point(137, 37);
+            this.dateTimePicker3.MaxDate = new System.DateTime(2100, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker3.MinDate = new System.DateTime(2015, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.ShowUpDown = true;
+            this.dateTimePicker3.Size = new System.Drawing.Size(125, 20);
+            this.dateTimePicker3.TabIndex = 4;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker2.Location = new System.Drawing.Point(6, 76);
+            this.dateTimePicker2.MaxDate = new System.DateTime(2100, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker2.MinDate = new System.DateTime(2015, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(125, 20);
+            this.dateTimePicker2.TabIndex = 3;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(6, 37);
+            this.dateTimePicker1.MaxDate = new System.DateTime(2100, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.MinDate = new System.DateTime(2015, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(125, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Do";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Od";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(206, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generuj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbxListMachine);
+            this.groupBox2.Location = new System.Drawing.Point(12, 135);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(269, 74);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Maszyna";
+            // 
+            // cbxListMachine
+            // 
+            this.cbxListMachine.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxListMachine.FormattingEnabled = true;
+            this.cbxListMachine.Location = new System.Drawing.Point(6, 19);
+            this.cbxListMachine.Name = "cbxListMachine";
+            this.cbxListMachine.Size = new System.Drawing.Size(255, 21);
+            this.cbxListMachine.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 246);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "TPZ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cbxListMachine;
+
+    }
+}
---
+++ RGolemAddinSLN/RGolemAddin/View/Form1.cs
@@ -0,0 +1,93 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+using RGolemAddin.Config;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace RGolemAddin.View
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            PopulateListMachine();
+        }
+
+        private void PopulateListMachine()
+        {
+            var dict = new Dictionary<int, string>();
+
+            FbTransaction transaction;
+            FbCommand command = new FbCommand();
+            DataTable dt = new DataTable();
+
+            using (FbConnection connection = new FbConnection(DataBaseConnection.GetConnectionString()))
+            {
+                connection.Open();
+
+                transaction = connection.BeginTransaction();
+                command.Transaction = transaction;
+                command.Connection = connection;
+                command.CommandText = @"select sv, maszyna from maszyny";
+                FbDataAdapter adapter = new FbDataAdapter(command);
+                adapter.Fill(dt);
+
+                connection.Close();
+            }
+
+            foreach (DataRow row in dt.Rows)
+            {
+                dict.Add((int)row["SV"], row["MASZYNA"].ToString());
+            }
+
+            cbxListMachine.DataSource = new BindingSource(dict, null);
+            cbxListMachine.DisplayMember = "Value";
+            cbxListMachine.ValueMember = "Key";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FbTransaction transaction;
+            FbCommand command = new FbCommand();
+            DataTable dt = new DataTable();
+
+            using (FbConnection connection = new FbConnection(DataBaseConnection.GetConnectionString()))
+            {
+                connection.Open();
+
+                transaction = connection.BeginTransaction();
+                command.Transaction = transaction;
+                command.Connection = connection;
+                command.CommandText = @"select sv, maszyna from maszyny order by sv";
+                FbDataAdapter adapter = new FbDataAdapter(command);
+                adapter.Fill(dt);
+
+                connection.Close();
+            }
+
+            Excel.Worksheet activeWorksheet = ((Excel.Worksheet)Globals.ThisAddIn.Application.ActiveSheet);
+
+
+            ((Excel.Range)activeWorksheet.Cells[1, 1]).Value2 = "SV";
+            ((Excel.Range)activeWorksheet.Cells[1, 2]).Value2 = "MASZYNA";
+            int i = 2;
+            foreach (DataRow row in dt.Rows)
+            {
+                ((Excel.Range)activeWorksheet.Cells[i, 1]).Value2 = row["SV"];
+                ((Excel.Range)activeWorksheet.Cells[i, 2]).Value2 = row["MASZYNA"];
+                i++;
+            }
+            ((Excel.Range)activeWorksheet.Cells[1, 2]).EntireColumn.AutoFit();
+
+            this.Close();
+        }
+    }
+}
